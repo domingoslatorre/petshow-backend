163dc49e703f26fea348ab43f45c1ed485e53e21
gaugsouza <guilhermeguis@outlook.com>
2020-03-08 23:16:48 -0300

    init

---
+++ src/main/java/hyve/petshow/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package hyve.petshow.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
