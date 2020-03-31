41b7bc1818d27661e2da55b4c7a1c5100994550e
gaugsouza <guilhermeguis@outlook.com>
2020-03-30 22:46:16 -0300

    #26 adicionado log4j2, ferramenta de log

--- src/main/java/hyve/petshow/demo/DemoApplication.java
+++ src/main/java/hyve/petshow/demo/DemoApplication.java
@@ -1,17 +1,23 @@
 package hyve.petshow.demo;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.*;
 
+
 @Controller
 @SpringBootApplication
 public class DemoApplication {
+	private static final Logger logger = LogManager.getLogger(DemoApplication.class);
 
 	@RequestMapping("/")
 	@ResponseBody
 	String home() {
+		logger.info("Hi");
 		return "Hello World!";
 	}
 
