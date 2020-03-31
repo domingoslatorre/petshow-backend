841e8fcb0a29c8a54c2d66bd3975cf53b26d136e
gaugsouza <guilhermeguis@outlook.com>
2020-03-30 21:23:36 -0300

    changed to springboot

---
+++ src/test/java/hyve/petshow/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package hyve.petshow.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
