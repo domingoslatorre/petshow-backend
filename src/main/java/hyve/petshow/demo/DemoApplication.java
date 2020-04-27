2fcebc135de4760d0f1ef8bcfdb41b438c8213c8
gaugsouza <guilhermeguis@outlook.com>
2020-04-27 10:41:27 -0300

    #46 Fixed Codacy issues

--- src/main/java/hyve/petshow/demo/DemoApplication.java
+++ src/main/java/hyve/petshow/demo/DemoApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Log4j2
 @Controller
