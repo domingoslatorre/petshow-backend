46c3f5d4a001cebb387fef4a9c7442f1168b6454
gaugsouza <guilhermeguis@outlook.com>
2020-04-27 10:31:09 -0300

    #46 Fixed Codacy issues

--- src/main/java/hyve/petshow/demo/DemoApplication.java
+++ src/main/java/hyve/petshow/demo/DemoApplication.java
@@ -4,14 +4,14 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Log4j2
 @Controller
 @SpringBootApplication
 public class DemoApplication {
-	@RequestMapping("/home")
+	@GetMapping("/home")
 	@ResponseBody
 	String home() {
 		log.info("Hi");
--- src/test/java/hyve/petshow/demo/DemoApplicationTests.java
+++ src/test/java/hyve/petshow/demo/DemoApplicationTests.java
@@ -3,7 +3,7 @@ package hyve.petshow.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class DemoApplicationTests {
