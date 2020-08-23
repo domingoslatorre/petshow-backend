c46b439f4f42f3aee8f686ec9adf70302cd7abe4
gaugsouza <guilhermeguis@outlook.com>
2020-08-23 16:50:29 -0300

    Correcao teste

--- src/test/java/hyve/petshow/ApplicationTests.java
+++ src/test/java/hyve/petshow/ApplicationTests.java
@@ -7,16 +7,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class ApplicationTests {
 	@Test
 	void test01() {
-		boolean teste = false;
-
-		assertEquals(true, teste);
-	}
-
-	@Test
-	void test02() {
 		boolean teste = true;
 
 		assertEquals(true, teste);
 	}
-
 }
