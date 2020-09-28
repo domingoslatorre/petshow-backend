3b2cf93b6f9bf0da873d189a88c887a94ed8cbd8
Thiago Xavier R. de Almeida <thiago.xra@gmail.com>
2020-09-28 00:20:03 -0300

    Criado Autonomo

--- src/main/java/hyve/petshow/repository/AutonomoRepository.java
+++ src/main/java/hyve/petshow/repository/AutonomoRepository.java
@@ -1,13 +1,11 @@
 package hyve.petshow.repository;
 
-import java.util.Optional;
-
-import hyve.petshow.domain.Prestador;
+import hyve.petshow.domain.Autonomo;
 import hyve.petshow.domain.Login;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
-public interface PrestadorRepository extends ContaRepository<Prestador> {
-    Optional<Prestador> findByLogin(Login login);
+public interface AutonomoRepository extends ContaRepository<Autonomo> {
+    Optional<Autonomo> findByLogin(Login login);
 }
