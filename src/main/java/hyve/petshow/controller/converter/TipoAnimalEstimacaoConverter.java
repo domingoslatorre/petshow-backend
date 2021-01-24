bddfb68b5b6bc81a821a0651c4d3ccc73fb025de
calebe <calebe.fazzani.facul@gmail.com>
2021-01-24 00:18:10 -0300

    Remoção de optional

--- src/main/java/hyve/petshow/controller/converter/TipoAnimalEstimacaoConverter.java
+++ src/main/java/hyve/petshow/controller/converter/TipoAnimalEstimacaoConverter.java
@@ -1,7 +1,5 @@
 package hyve.petshow.controller.converter;
 
-import java.util.Optional;
-
 import org.springframework.stereotype.Component;
 
 import hyve.petshow.controller.representation.TipoAnimalEstimacaoRepresentation;
@@ -11,26 +9,22 @@ import hyve.petshow.domain.TipoAnimalEstimacao;
 public class TipoAnimalEstimacaoConverter implements Converter<TipoAnimalEstimacao, TipoAnimalEstimacaoRepresentation> {
     @Override
     public TipoAnimalEstimacaoRepresentation toRepresentation(TipoAnimalEstimacao domain) {
-    	return Optional.ofNullable(domain).map(dominio -> {
-    		var representation = new TipoAnimalEstimacaoRepresentation();
-    		representation.setId(domain.getId());
-            representation.setNome(domain.getNome());
-            representation.setPelagem(domain.getPelagem());
-            representation.setPorte(domain.getPorte());
-    		return representation;
-    	}).orElse(new TipoAnimalEstimacaoRepresentation()); 
+    	var representation = new TipoAnimalEstimacaoRepresentation();
+		representation.setId(domain.getId());
+        representation.setNome(domain.getNome());
+        representation.setPelagem(domain.getPelagem());
+        representation.setPorte(domain.getPorte());
+		return representation;
     }
 
     @Override
     public TipoAnimalEstimacao toDomain(TipoAnimalEstimacaoRepresentation representation) {
-    	return Optional.ofNullable(representation).map(representacao -> {
-    		var domain = new TipoAnimalEstimacao();
+    	var domain = new TipoAnimalEstimacao();
 
-            domain.setId(representacao.getId());
-            domain.setNome(representacao.getNome());
-            domain.setPelagem(representacao.getPelagem());
-            domain.setPorte(representacao.getPorte());
-            return domain;
-    	}).orElse(new TipoAnimalEstimacao());
+        domain.setId(representation.getId());
+        domain.setNome(representation.getNome());
+        domain.setPelagem(representation.getPelagem());
+        domain.setPorte(representation.getPorte());
+        return domain;
     }
 }
