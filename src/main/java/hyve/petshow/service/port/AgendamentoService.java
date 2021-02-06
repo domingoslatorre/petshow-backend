7706db845d178acfa69d1b271989fd1a068e4b46
calebe <calebe.fazzani.facul@gmail.com>
2021-02-06 18:40:08 -0300

    Adição de funcionaidade de retorno de horários já agendados - #332

--- src/main/java/hyve/petshow/service/port/AgendamentoService.java
+++ src/main/java/hyve/petshow/service/port/AgendamentoService.java
@@ -1,12 +1,16 @@
 package hyve.petshow.service.port;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
 import hyve.petshow.domain.Agendamento;
 import hyve.petshow.domain.StatusAgendamento;
 import hyve.petshow.exceptions.BusinessException;
 import hyve.petshow.exceptions.NotFoundException;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
 
 @Service
 public interface AgendamentoService {
@@ -21,4 +25,7 @@ public interface AgendamentoService {
     Agendamento atualizarAgendamento(Long id, Long prestadorId, Agendamento request) throws BusinessException, NotFoundException;
 
     Agendamento atualizarStatusAgendamento(Long id, Long prestadorId, StatusAgendamento statusAgendamento) throws BusinessException, NotFoundException;
+    
+    List<String> buscarHorariosAgendamento(Long prestadorId, LocalDate dataAgendamento);
+
 }
